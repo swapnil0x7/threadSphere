22aa279cec1502a43f239ae88b8770194c90dd1e
swapnil0x7 <swapnil0x7@gmai.com>
2024-06-30 02:36:09 +0530

    feat: developed UserHeader UI

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import { Button, Container } from '@chakra-ui/react';
+import UserPage from './pages/UserPage';
+import PostPage from './pages/PostPage';
+import Header from './components/Header';
+
+function App() {
+	return (
+		<Container maxW='620px'>
+			<Header />
+			<Routes>
+				<Route path='/:username' element={<UserPage />} />
+				<Route path='/:username/post/:pid' element={<PostPage />} />
+			</Routes>
+		</Container>
+	);
+}
+
+export default App;
